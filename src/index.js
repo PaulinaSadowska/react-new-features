4615f4b043c080aea6789a98c8706670ae7732bb
palka <7paulina.sadowska@gmail.com>
2021-09-02 21:14:57 +0200

    Note app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { CalculatorApp } from './playground/CalculatorApp'
+
+const NoteApp = () => {
+
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+  const saveNote = (e) => {
+    e.preventDefault()
+    setNotes([...notes, { title, body }])
+    setTitle("")
+    setBody("")
+  }
+
+  const removeNote = (title) => {
+    const filteredNotes = notes.filter((note) => note.title !== title)
+    setNotes(filteredNotes)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {
+        notes.map((note) => (
+          <div key={note.title}>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => removeNote(note.title)}>x</button>
+          </div>
+        ))
+      }
+      <p>add note:</p>
+      <form onSubmit={saveNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <input value={body} onChange={(e) => setBody(e.target.value)}></input>
+        <button>add note</button>
+      </form>
+    </div>
+  )
+}
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CalculatorApp initialValue={9} />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
